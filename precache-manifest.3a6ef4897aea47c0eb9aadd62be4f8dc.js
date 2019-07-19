23044e5a0200cdd7c15c6f7c394ab3c84edfda6e
Yevgeniy <zleonenkov@gmail.com>
2019-07-19 15:04:29 +0300

    Updates

---
+++ precache-manifest.3a6ef4897aea47c0eb9aadd62be4f8dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7a98ecaff87cb35d122e1c971e33f19",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "60a868b8581e207adffb",
+    "url": "/react_add-todo-form/static/css/main.7ad3fae4.chunk.css"
+  },
+  {
+    "revision": "8f87b985d6595e9289f7",
+    "url": "/react_add-todo-form/static/js/2.3a7722c9.chunk.js"
+  },
+  {
+    "revision": "60a868b8581e207adffb",
+    "url": "/react_add-todo-form/static/js/main.bcd2465a.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
